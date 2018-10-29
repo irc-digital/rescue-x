10d5eab5b294c35d16ade69070fe7b3cfc14b605
andythornton <me@andy-j-thornton.com>
2018-10-29 13:10:06 -0700

    x

--- web/themes/x/dist/source/_patterns/05-components/80-highlight/highlight.js
+++ web/themes/x/dist/source/_patterns/05-components/80-highlight/highlight.js
@@ -1,7 +1,12 @@
 (function ($) {
   Drupal.behaviors.showHideHighlightToggle = {
     attach: function attach(context, settings) {
-      console.debug ('hi');
+      $(".rplc-highlight__show-hide").click(function(e){
+        var highlight_embeddable = $(this).parents('.rplc-highlight');
+        highlight_embeddable.attr('data-rpl-state', highlight_embeddable.attr('data-rpl-state') == 'expanded' ? 'collapsed' : 'expanded');
+
+        e.preventDefault();
+      });
     }
   };
 })(jQuery);
