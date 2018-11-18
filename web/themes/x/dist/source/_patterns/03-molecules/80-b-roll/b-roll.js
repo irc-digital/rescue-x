0744237b406201b05f693cc8e063bb04f0dbc93d
andythornton <me@andy-j-thornton.com>
2018-11-18 13:01:31 -0800

    x

--- web/themes/x/dist/source/_patterns/03-molecules/80-b-roll/b-roll.js
+++ web/themes/x/dist/source/_patterns/03-molecules/80-b-roll/b-roll.js
@@ -3,41 +3,45 @@
     attach: function attach(context, settings) {
       var device = window.innerWidth > 640 ? 'desktop' : 'mobile';
       var attribute_name = 'data-rpl-b-roll-src-' + device;
-      var video_element = $('[' + attribute_name + ']');
-      var video_url = video_element.attr(attribute_name);
 
-      if (typeof video_url == "string") {
-        video_element = video_element[0];
-        var source = document.createElement('source');
-        source.setAttribute('src', video_url);
-        video_element.appendChild(source);
+      $('[' + attribute_name + ']').each(function(){
+        var video_element = $(this);
+        var video_url = video_element.attr(attribute_name);
 
-        $(video_element).one('canplaythrough', function() {
-          var parent = $(this).parent();
-          var target = parent;
-          var classes = $(this).attr('class').split(/\s+/);
+        if (typeof video_url == "string") {
+          video_element = video_element[0];
+          var source = document.createElement('source');
+          source.setAttribute('src', video_url);
+          video_element.appendChild(source);
 
-          if (classes.length > 0) {
-            var first_class = classes[0];
-            var position_of_bem_separator = first_class.indexOf("__");
+          $(video_element).one('canplaythrough', function() {
+            var parent = $(this).parent();
+            var target = parent;
+            var classes = $(this).attr('class').split(/\s+/);
 
-            if (position_of_bem_separator) {
-              var bem_element = first_class.substring(0, position_of_bem_separator);
+            if (classes.length > 0) {
+              var first_class = classes[0];
+              var position_of_bem_separator = first_class.indexOf("__");
 
-              var ancestor_element = $(this).parents('.' + bem_element);
+              if (position_of_bem_separator) {
+                var bem_element = first_class.substring(0, position_of_bem_separator);
 
-              if (typeof ancestor_element !== 'undefined') {
-                target = ancestor_element;
+                var ancestor_element = $(this).parents('.' + bem_element);
+
+                if (typeof ancestor_element !== 'undefined') {
+                  target = ancestor_element;
+                }
               }
             }
-          }
 
-          target.attr('data-rpl-b-roll-loaded', '');
-          window.setTimeout(function(){ video_element.play(); }, 2000);
-        });
-        video_element.load();
+            target.attr('data-rpl-b-roll-loaded', '');
+            window.setTimeout(function(){ video_element.play(); }, 2000);
+          });
+          video_element.load();
 
-      }
+        }
+      });
+
     }
   };
 })(jQuery);
