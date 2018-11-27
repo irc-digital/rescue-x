ccb7f31000bc26ea26202dfec6d86534c949ead5
andythornton <me@andy-j-thornton.com>
2018-11-27 09:26:43 -0800

    x

--- web/themes/x/dist/source/_patterns/05-components/92-menu/05-main/main-menu.js
+++ web/themes/x/dist/source/_patterns/05-components/92-menu/05-main/main-menu.js
@@ -1,8 +1,8 @@
 (function ($) {
   Drupal.behaviors.rplMainMenu = {
     attach: function attach(context, settings) {
-      $("[data-rpl-main-menu-expanding-section]").click(function(e){
-        if ($(this).parent()[0].hasAttribute('data-rpl-main-menu-expanded-section-target')) {
+      $("[data-rpl-main-menu-expanding-section-target]").click(function(e){
+        if ($(this).parent()[0].hasAttribute('data-rpl-main-menu-expanded-section')) {
           $(this).parent().removeAttr('data-rpl-main-menu-expanded-section');
         } else {
           $(this).parent().parent().children().removeAttr('data-rpl-main-menu-expanded-section');
