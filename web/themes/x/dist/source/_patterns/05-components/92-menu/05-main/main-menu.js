9e5de34f6c41b8ccd5d40c59fdac5287df74959a
andythornton <me@andy-j-thornton.com>
2018-11-27 20:39:44 -0800

    x

--- web/themes/x/dist/source/_patterns/05-components/92-menu/05-main/main-menu.js
+++ web/themes/x/dist/source/_patterns/05-components/92-menu/05-main/main-menu.js
@@ -8,7 +8,45 @@
           $(this).parent().parent().children().removeAttr('data-rpl-main-menu-expanded-section');
           $(this).parent().attr('data-rpl-main-menu-expanded-section', '');
         }
+
+        if (window.innerWidth <= 1142) {
+          var target = $(this);
+          setTimeout(function (){
+            var new_target = target;
+
+            var child_adjustment = 0;
+
+            if (target.parent()[0].hasAttribute('data-rpl-main-menu-level-3')) {
+              new_target = $(target).parents('[data-rpl-main-menu-level-1]').find('[data-rpl-main-menu-expanding-section-target]');
+              child_adjustment = $(target).parent().position().top; //+ $(target).parent().parent().position().top; // + $(target).parent().parent().parent().position().top;
+            }
+
+            var scroll_to = $(new_target).position().top + $(new_target).parent().position().top - $(new_target).parent().parent().position().top + child_adjustment;
+
+            var scroll_target = $('html, body');
+
+            $(target).parents().each(function() {
+              var element = $(this);
+
+              if (element.css('position') == 'fixed') {
+                scroll_target = element;
+                return false;
+              }
+
+              if ($(this).prop("tagName") == 'BODY') {
+                return false;
+              }
+            });
+
+            scroll_target.animate({
+              scrollTop: scroll_to
+            }, 1000);
+
+          }, 300);
+        }
       });
     }
   };
+
+
 })(jQuery);
