b43a5876948a3c09561b60f00cb6fe812ea33181
andythornton <me@andy-j-thornton.com>
2018-12-04 02:32:41 -0800

    x

--- web/themes/x/dist/source/_patterns/05-components/77-modal/modal.js
+++ web/themes/x/dist/source/_patterns/05-components/77-modal/modal.js
@@ -5,11 +5,21 @@
         var modal_id = $(this).attr("data-rpl-open-modal");
         rplOpenModalWithId(modal_id);
       });
+
+      $('[data-rpl-modal-close]').click(function(e) {
+        rplCloseModal();
+      });
+
+      $(document).keyup(function(e) {
+        if (e.keyCode === 27) {
+          rplCloseModal();
+        }
+      });
     }
   };
 
   function rplOpenModalWithId (modal_id) {
-    rplOpenModal ($('[data-rpl-modal-id]'));
+    rplOpenModal ($('[data-rpl-modal-id="' + modal_id + '"]'));
   }
 
   function rplOpenModal (modal) {
@@ -18,11 +28,9 @@
   }
 
   function rplCloseModal () {
-
+    $('body').removeAttr("data-rpl-modal-opened");
+    $('[data-rpl-modal-state="opened"]').attr('data-rpl-modal-state', 'closed');
   }
 
-  function rplOpenModalWithContent () {
-
-  }
 
 })(jQuery);
