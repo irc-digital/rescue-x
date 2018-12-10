75d8cbbf85577fd5aec12e7fcf029e1be5a5c57c
Russ Bomhof <russ.bomhof@rescue.org>
2018-12-10 12:00:08 -0500

    x

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
@@ -8,8 +8,12 @@
                 nextArrow: ".rpll-slider-layout-1__next",
                 dotsClass: "rpll-slider-layout-1__pager",
                 customPaging: function (slick, i) {
-                    var title = slick.$slides.eq(i).find('.rplc-teaser-story').attr('data-rpl-teaser-story-nav-title');
-                    return '<div>' + title + '</div>';
+                    var story = slick.$slides.eq(i).find('.rplc-teaser-story');
+                    var title = story.attr("data-rpl-teaser-story-nav-title");
+                    var description = story.attr("data-rpl-teaser-story-nav-description");
+                    var navigation = '<div class="description">' + description + '</div>';
+                    navigation += '<div class="title">' + title + '</div>';
+                    return navigation;
                 },
             });
         }
