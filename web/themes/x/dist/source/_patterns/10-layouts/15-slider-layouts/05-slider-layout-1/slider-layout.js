35a35d868bb5312062ecf51f59109d08402922ee
Russ Bomhof <russ.bomhof@rescue.org>
2018-12-10 16:56:41 -0500

    broken slider

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
@@ -2,11 +2,6 @@
     Drupal.behaviors.rplSliders = {
         attach: function attach(context, settings) {
             $('.rpll-slider-layout-1__slides').slick({
-                dots: true,
-                useTransform: false,
-                prevArrow: ".rpll-slider-layout-1__previous",
-                nextArrow: ".rpll-slider-layout-1__next",
-                dotsClass: "rpll-slider-layout-1__pager",
                 customPaging: function (slick, i) {
                     var story = slick.$slides.eq(i).find('.rplc-teaser-story');
                     var title = story.attr("data-rpl-teaser-story-nav-title");
@@ -14,7 +9,7 @@
                     var navigation = '<div class="description">' + description + '</div>';
                     navigation += '<div class="title">' + title + '</div>';
                     return navigation;
-                },
+                }
             });
         }
     };
