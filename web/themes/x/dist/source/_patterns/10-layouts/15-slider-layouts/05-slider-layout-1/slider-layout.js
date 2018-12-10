cc115190797945b6de84071d0172d86bd264446b
Russ Bomhof <russ.bomhof@rescue.org>
2018-12-10 11:32:20 -0500

    x

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
@@ -1,13 +1,12 @@
 (function ($) {
     Drupal.behaviors.rplSliders = {
         attach: function attach(context, settings) {
-            console.log("test");
             $('.rpll-slider-layout-1__slides').slick({
                 dots: true,
                 useTransform: false,
                 prevArrow: ".rpll-slider-layout-1__previous",
                 nextArrow: ".rpll-slider-layout-1__next",
-                dotsClass: ".rpll-slider-layout-1__pager",
+                dotsClass: "rpll-slider-layout-1__pager",
                 customPaging: function (slick, i) {
                     var title = slick.$slides.eq(i).find('.rplc-teaser-story').attr('data-rpl-teaser-story-nav-title');
                     return '<div>' + title + '</div>';
