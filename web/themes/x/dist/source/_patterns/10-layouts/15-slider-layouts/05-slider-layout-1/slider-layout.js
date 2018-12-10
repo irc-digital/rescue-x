68eb12870f594eb80420de303693ade01659b372
Russ Bomhof <russ.bomhof@rescue.org>
2018-12-10 11:26:18 -0500

    start on slide nav

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/05-slider-layout-1/slider-layout.js
@@ -1,7 +1,18 @@
 (function ($) {
     Drupal.behaviors.rplSliders = {
         attach: function attach(context, settings) {
-            $('.rpll-slider-layout-1__slides').slick();
+            console.log("test");
+            $('.rpll-slider-layout-1__slides').slick({
+                dots: true,
+                useTransform: false,
+                prevArrow: ".rpll-slider-layout-1__previous",
+                nextArrow: ".rpll-slider-layout-1__next",
+                dotsClass: ".rpll-slider-layout-1__pager",
+                customPaging: function (slick, i) {
+                    var title = slick.$slides.eq(i).find('.rplc-teaser-story').attr('data-rpl-teaser-story-nav-title');
+                    return '<div>' + title + '</div>';
+                },
+            });
         }
     };
 })(jQuery);
