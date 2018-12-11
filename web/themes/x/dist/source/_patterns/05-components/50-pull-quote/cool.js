a28cbac01ed1a7c2d95d3139049328cca5a9ee9c
andythornton <me@andy-j-thornton.com>
2018-12-11 05:50:05 -0800

    x

--- web/themes/x/dist/source/_patterns/05-components/50-pull-quote/cool.js
+++ web/themes/x/dist/source/_patterns/05-components/50-pull-quote/cool.js
@@ -2,10 +2,16 @@
   Drupal.behaviors.rplCool = {
     attach: function attach(context, settings) {
       $("[data-cool]").each(function() {
-        console.debug('daddy ... daddy cool');
         var element = $(this);
-        console.debug (element.attr('data-cool'));
-        element.append(element.attr('data-cool'));
+        var pager_items = element.attr('data-cool');
+        var wrapper = document.createElement('div');
+        wrapper.innerHTML = pager_items;
+
+        for (var i = 0; i < wrapper.childElementCount; i++) {
+          var pager_item = wrapper.children[i];
+          element.append(pager_item.cloneNode(true));
+        }
+        
         element.removeAttr('data-cool');
       });
     }
