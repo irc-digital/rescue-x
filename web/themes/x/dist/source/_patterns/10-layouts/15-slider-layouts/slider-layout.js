5498bf56594d0781ef66f198f107a788085153f3
andythornton <me@andy-j-thornton.com>
2018-12-11 08:29:38 -0800

    x

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
@@ -1,22 +1,22 @@
 (function ($) {
     Drupal.behaviors.rplSliders = {
         attach: function attach(context, settings) {
-            $('[data-rpl-slider-slides]').each(function(key, item) {
-              var dots_class = this.getAttribute('data-rpl-slider-pager');
-              var use_dots = dots_class != null;
+          $('[data-rpl-slider-slides]').each(function(key, item) {
+            var dots_class = this.getAttribute('data-rpl-slider-pager');
+            var use_dots = dots_class != null;
 
-              $(this).slick({
-                dots: use_dots,
-                dotsClass: dots_class,
-                useTransform: false,
-                prevArrow: "[data-rpl-slider-previous]",
-                nextArrow: "[data-rpl-slider-next]",
-                customPaging: function (slick, i) {
-                  var slide = slick.$slides.eq(i).find('[data-rpl-slider-slide-pager-item]');
-                  return slide[0].getAttribute('data-rpl-slider-slide-pager-item');
-                }
-              });
+            $(this).slick({
+              dots: use_dots,
+              dotsClass: dots_class,
+              useTransform: false,
+              prevArrow: "[data-rpl-slider-previous]",
+              nextArrow: "[data-rpl-slider-next]",
+              customPaging: function (slick, i) {
+                var slide = slick.$slides.eq(i).find('[data-rpl-slider-slide-pager-item]');
+                return slide[0].getAttribute('data-rpl-slider-slide-pager-item');
+              }
             });
+          });
         }
     };
 })(jQuery);
