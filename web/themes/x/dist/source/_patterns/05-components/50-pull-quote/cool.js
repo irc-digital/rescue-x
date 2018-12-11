5c9a87896626af688fab2fe3dbb4eb5ea2e496b9
andythornton <me@andy-j-thornton.com>
2018-12-10 20:10:59 -0800

    x

---
+++ web/themes/x/dist/source/_patterns/05-components/50-pull-quote/cool.js
@@ -0,0 +1,13 @@
+(function ($) {
+  Drupal.behaviors.rplCool = {
+    attach: function attach(context, settings) {
+      $("[data-cool]").each(function() {
+        console.debug('daddy ... daddy cool');
+        var element = $(this);
+        console.debug (element.attr('data-cool'));
+        element.append(element.attr('data-cool'));
+      });
+    }
+  };
+
+})(jQuery);
