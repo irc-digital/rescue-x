5e5443acf6b8adafbdd683170d356ca8c9cc2267
andythornton <me@andy-j-thornton.com>
2018-12-12 09:53:10 -0800

    x

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
@@ -4,17 +4,21 @@
             $('[data-rpl-slider-slides]').each(function(key, item) {
                 var dots_class = this.getAttribute('data-rpl-slider-pager');
                 var use_dots = dots_class != null;
+                var slides_to_show = this.getAttribute('data-rpl-slider-slides-to-show');
+                slides_to_show = slides_to_show != null ? slides_to_show : '1';
+
                 $(this).slick({
-                    dots: use_dots,
-                    dotsClass: dots_class,
-                    useTransform: false,
-                    touchThreshold: 10,
-                    prevArrow: $('[data-rpl-slider-previous]')[key],
-                    nextArrow: $('[data-rpl-slider-next]')[key],
-                    customPaging: function (slick, i) {
-                        var slide = slick.$slides.eq(i).find('[data-rpl-slider-slide-pager-item]');
-                        return slide[0].getAttribute('data-rpl-slider-slide-pager-item');
-                    }
+                  dots: use_dots,
+                  dotsClass: dots_class,
+                  useTransform: false,
+                  slidesToShow: slides_to_show,
+                  touchThreshold: 10,
+                  prevArrow: $('[data-rpl-slider-previous]')[key],
+                  nextArrow: $('[data-rpl-slider-next]')[key],
+                  customPaging: function (slick, i) {
+                      var slide = slick.$slides.eq(i).find('[data-rpl-slider-slide-pager-item]');
+                      return slide[0].getAttribute('data-rpl-slider-slide-pager-item');
+                  }
                 });
             });
         }
