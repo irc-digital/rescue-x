8cd2dab42ed6172dfdc79e1719eb09d80f62e815
andythornton <me@andy-j-thornton.com>
2018-12-13 12:22:30 -0800

    x

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
@@ -2,11 +2,6 @@
     Drupal.behaviors.rplSliders = {
         attach: function attach(context, settings) {
             $('[data-rpl-slider-slides]').each(function(key, item) {
-
-                var getDataOptions = function ( options ) {
-                    return (!options || typeof JSON.parse !== 'function') ? {} : JSON.parse(options);
-                };
-
                 var use_dots = this.hasAttribute('data-rpl-slider-pager');
                 var dots_class = this.getAttribute('data-rpl-slider-pager');
 
@@ -19,14 +14,9 @@
                   dots_class = component_class + '__pager';
                 }
 
-                console.debug(this.getAttribute('data-rpl-slider-responsive'));
-
                 var slides_to_show = this.getAttribute('data-rpl-slider-slides-to-show');
-                var responsive_settings = getDataOptions( item ? this.getAttribute('data-rpl-slider-responsive') : null );
-
-                console.debug(responsive_settings);
+                var responsive_settings = JSON.parse(this.getAttribute('data-rpl-slider-responsive'));
                 slides_to_show = slides_to_show != null ? slides_to_show : '1';
-
                 var infinite = slides_to_show == 1;
 
                 $(this).slick({
