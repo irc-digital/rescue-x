0df7e68f89e5b93d7114ae72cd7d1d23c84cc30a
andythornton <me@andy-j-thornton.com>
2018-12-12 19:31:28 -0800

    x

--- web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
+++ web/themes/x/dist/source/_patterns/10-layouts/15-slider-layouts/slider-layout.js
@@ -7,8 +7,18 @@
                     return (!options || typeof JSON.parse !== 'function') ? {} : JSON.parse(options);
                 };
 
+                var use_dots = this.hasAttribute('data-rpl-slider-pager');
                 var dots_class = this.getAttribute('data-rpl-slider-pager');
-                var use_dots = dots_class != null;
+
+                if (use_dots && dots_class.length == 0) {
+                  // default to a pager class of component name __pager 
+                  var classes = this.getAttribute('class').split(/\s+/);
+                  var main_class = classes[0];
+                  var position_of_bem_separator = main_class.indexOf("__");
+                  var component_class = main_class.substring(0, position_of_bem_separator);
+                  dots_class = component_class + '__pager';
+                }
+
                 var slides_to_show = this.getAttribute('data-rpl-slider-slides-to-show');
                 var responsiveSettings = getDataOptions( item ? this.getAttribute('data-rpl-slider-responsive') : null );
                 slides_to_show = slides_to_show != null ? slides_to_show : '1';
