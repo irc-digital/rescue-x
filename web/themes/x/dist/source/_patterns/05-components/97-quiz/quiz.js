fe0a8983788e4a0851bca94eca9a5709c7833806
andythornton <me@andy-j-thornton.com>
2018-12-20 20:24:00 -0800

    x

--- web/themes/x/dist/source/_patterns/05-components/97-quiz/quiz.js
+++ web/themes/x/dist/source/_patterns/05-components/97-quiz/quiz.js
@@ -2,7 +2,18 @@
   Drupal.behaviors.quizSelection = {
     attach: function attach(context, settings) {
       $("[name='quiz']").click(function(e){
-        $(this).parents('[data-rpl-quiz-question-state]').attr('data-rpl-quiz-question-state', $(this).val());
+        var question = $(this).parents('[data-rpl-quiz-question-state]');
+
+        question.attr('data-rpl-quiz-question-state', $(this).val());
+
+        // show the next question
+        if ($(this).val() == 'correct') {
+          var next_question = question.next();
+
+          if (next_question.length == 1 && next_question[0].getAttribute('data-rpl-quiz-question-state') == 'hidden') {
+            next_question.attr('data-rpl-quiz-question-state', 'unanswered');
+          }
+        }
       });
     }
   };
