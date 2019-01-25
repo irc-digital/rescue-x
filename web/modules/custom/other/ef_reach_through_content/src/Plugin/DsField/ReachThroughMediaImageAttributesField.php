d6df7b9c45f02fc30a49f86202e268162e1c3a94
andythornton <me@andy-j-thornton.com>
2019-01-25 14:58:24 -0800

    x

--- web/modules/custom/other/ef_reach_through_content/src/Plugin/DsField/ReachThroughMediaImageAttributesField.php
+++ web/modules/custom/other/ef_reach_through_content/src/Plugin/DsField/ReachThroughMediaImageAttributesField.php
@@ -2,7 +2,12 @@
 
 namespace Drupal\ef_reach_through_content\Plugin\DsField;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Utility\LinkGeneratorInterface;
 use Drupal\ef_patterns\Plugin\DsField\MediaImageAttributesField;
+use Drupal\ef_reach_through_content\ReachThroughServiceInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a DS field that knows how to reach through the entity reference to
@@ -14,20 +19,49 @@ use Drupal\ef_patterns\Plugin\DsField\MediaImageAttributesField;
  * )
  */
 class ReachThroughMediaImageAttributesField extends MediaImageAttributesField {
+  /** @var \Drupal\ef_reach_through_content\ReachThroughServiceInterface */
+  protected $reachThroughService;
+
+  /**
+   * Constructs a Display Suite field plugin.
+   */
+  public function __construct($configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, LinkGeneratorInterface $link_generator, AccountInterface $current_user, ReachThroughServiceInterface $reachThroughService) {
+    $this->reachThroughService = $reachThroughService;
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $link_generator, $current_user);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.manager'),
+      $container->get('link_generator'),
+      $container->get('current_user'),
+      $container->get('ef.reach_through_service')
+    );
+  }
 
   protected function getEntityWithImageField() {
     $reach_through_entity = $this->entity();
 
-    $outer_entity = $reach_through_entity->reach_through_ref->entity;
-
-    $reach_through_fields = \Drupal::service('ef.reach_through_service')->getReachThoughtFieldMappings($reach_through_entity);
-    
-//    $outer_entity = $this->entity();
-
     $config = $this->getConfiguration();
     $field_name = $config['field']['field_name'];
-    $outer_field_name = $reach_through_fields[$field_name];
-    $media_image_entity = $outer_entity->{$outer_field_name}->entity;
+
+    if (isset($reach_through_entity->{$field_name}->entity)) {
+      // overridden image on the curated wrapper
+      $media_image_entity = $reach_through_entity->{$field_name}->entity;
+    } else {
+      // use image on underlying node
+      $outer_entity = $reach_through_entity->reach_through_ref->entity;
+
+      $reach_through_fields = $this->reachThroughService->getReachThoughtFieldMappings($reach_through_entity);
+      $outer_field_name = $reach_through_fields[$field_name];
+      $media_image_entity = $outer_entity->{$outer_field_name}->entity;
+    }
 
     return $media_image_entity;
   }
