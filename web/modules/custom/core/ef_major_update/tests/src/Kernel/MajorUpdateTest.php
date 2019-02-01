b8b16ca356c1a4e8f2a3c066914315aee2e359c0
Russ Bomhof <russ.bomhof@rescue.org>
2019-02-01 12:24:51 -0500

    test

---
+++ web/modules/custom/core/ef_major_update/tests/src/Kernel/MajorUpdateTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\Tests\ef_major_update;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Provide some basic tests for our FruitForm form.
+ * @group ef
+ */
+class MajorUpdateTest extends KernelTestBase {
+
+  /**
+   * Modules to install.
+   * @var array
+   */
+  public static $modules = ['language', 'content_translation', 'node', 'ef', 'ef_major_update', 'ef_major_update_test'];
+
+
+  public function setUp() {
+    parent::setUp();
+    $this->installConfig(['ef_major_update', 'ef_major_update_test']);
+  }
+
+  public function testMajorlyUpdatedFieldIsEmptyforNewNodesWithFieldOffForm() {
+    $this->drupalCreateNode(['type' => 'test_field_off_form', 'status' => TRUE]);
+  }
+}
