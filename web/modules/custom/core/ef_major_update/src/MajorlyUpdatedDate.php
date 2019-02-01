751b427f1a851e0ae34872eac7e84cf7495e4e0a
andythornton <me@andy-j-thornton.com>
2019-01-31 19:21:58 -0800

    review

--- web/modules/custom/core/ef_major_update/src/MajorlyUpdatedDate.php
+++ web/modules/custom/core/ef_major_update/src/MajorlyUpdatedDate.php
@@ -2,26 +2,42 @@
 
 namespace Drupal\ef_major_update;
 
-class MajorlyUpdatedDate {
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class MajorlyUpdatedDate implements ContainerInjectionInterface {
+
+  /**
+   * @var TimeInterface
+   */
+  protected $time;
+
+  public function __construct(TimeInterface $time) {
+    $this->time = $time;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('datetime.time')
+    );
+  }
+
   /**
    * Set the majorly updated timestamp
    *
    * @param $node
    */
-  public function updateDate($node) {
-
-    if($node->original) {
-      $previous_major_update = $node->original->get('field_majorly_updated')->getValue();
+  public function updateDate(NodeInterface $node) {
+    if($node->major_update) {
+      // editor has marked this as a major change - set the majorly_updated field
+      // with the request timestamp (this will ensure it matched the changed field)
+      $node->field_majorly_updated = $this->time->getRequestTime();
+    } else if (!$node->id()) {
+      // first version - because of defaulting in the timestamp area we actually
+      // want to set this to null. This makes life easier when we render
+      $node->field_majorly_updated = NULL;
     }
-
-    // Only set timestamp for major update.
-    if(isset($_POST["major_update"])) {
-      $node->set('field_majorly_updated', time());
-    } else {
-      if(!isset($previous_major_update[0]['value'])) {
-        $node->set('field_majorly_updated', null);
-      }
-    }
-
   }
 }
